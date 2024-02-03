91c1f803026fd67028db44d3e47bc18386afa2fb
dzhager3354 <andrey.kondrashov.04@mail.ru>
2024-02-03 18:19:39 +0300

    test

--- src/main/java/ru/dzhager3354/Main.java
+++ src/main/java/ru/dzhager3354/Main.java
@@ -4,6 +4,7 @@ import ru.dzhager3354.window.Window;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("test");
         new Thread(new Window()).start();
     }
 }
